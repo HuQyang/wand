5b6f739e2395422a52a77eea0b3e89ef416460c3
HuQYang <whiary1201@gmail.com>
2025-07-23 13:21:47 +0200

    new

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from douzero.dmc import parser, train
 
 if __name__ == '__main__':
     flags = parser.parse_args()
-    flags.num_actors = 28
+    flags.num_actors = 20
     flags.num_threads = 190
     flags.load_model = True
     flags.batch_size = 32
