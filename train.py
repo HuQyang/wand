70b5ac8661bb905072ec71a5193c337e22fa23e5
HuQYang <whiary1201@gmail.com>
2025-07-23 13:14:01 +0200

    new

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from douzero.dmc import parser, train
 
 if __name__ == '__main__':
     flags = parser.parse_args()
-    flags.num_actors = 25
-    flags.num_threads = 64
+    flags.num_actors = 28
+    flags.num_threads = 190
     flags.load_model = True
     flags.batch_size = 32
     flags.savedir = "oracle_reward"
