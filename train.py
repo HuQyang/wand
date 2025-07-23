d9cb0f55b010ebd56af7c6a78563e5660cabbbe6
HuQYang <whiary1201@gmail.com>
2025-07-23 13:47:13 +0200

    new

--- train.py
+++ train.py
@@ -4,11 +4,12 @@ from douzero.dmc import parser, train
 
 if __name__ == '__main__':
     flags = parser.parse_args()
-    flags.num_actors = 4
-    flags.num_threads = 64
+    flags.num_actors = 16
+    flags.num_threads = 60
     flags.load_model = True
     flags.batch_size = 32
     flags.savedir = "oracle_reward"
-    flags.use_oracle_reward = False
+    flags.use_oracle_reward = True
+    flags.save_interval = 10
     # os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     train(flags)
